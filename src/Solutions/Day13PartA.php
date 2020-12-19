80af32cc3f7aab270aa54060cabf79bcce252738
Sean Nessworthy <sean@nessworthy.me>
2020-12-19 14:04:02 +0000

    13a, finally!

---
+++ src/Solutions/Day13PartA.php
@@ -0,0 +1,56 @@
+<?php declare(strict_types=1);
+
+namespace Nessworthy\AoC2020\Solutions;
+
+use Nessworthy\AoC2020\Common\Input;
+use Nessworthy\AoC2020\Common\Output;
+
+class Day13PartA implements Solution
+{
+
+    public function __construct()
+    {
+    }
+
+    private function removeOutOfServiceBusses($value): bool
+    {
+        return $value !== 'x';
+    }
+
+    public function modN($number, $mod): int
+    {
+        return $number % $mod;
+    }
+
+    public function execute(Input $input, Output $output): int|string
+    {
+        $lines = $input->readLine();
+        $time = (int) $lines->current();
+
+        $fn = curry([$this, 'modN'], $time);
+        $output->writeLine((string) $fn(939));
+
+        $lines->next();
+        $busses = explode(',', $lines->current());
+
+        // Filter out busses with 'x' as they're out of service, or so we think.
+        $busses = array_filter($busses, [$this, 'removeOutOfServiceBusses']);
+
+        // Set the array keys of each bus to the bus ID.
+        $busses = array_combine($busses, $busses);
+
+        // For each bus, determine the number of minutes between the current time and the last time the bus ran.
+        $busTimes = array_map(curry([$this, 'modN'], $time), $busses);
+
+        // Then, for each bus, subtract the bus ID to determine the number of minutes after the current time the bus is due to arrive.
+        foreach ($busTimes as $id => $time) {
+            $busTimes[$id] = $id - $time;
+        }
+
+        // Sort all the bus times by their time-to-arrival, and pick the earliest one.
+        asort($busTimes);
+        $index = array_key_first($busTimes);
+
+        return $index * $busTimes[$index];
+    }
+}
--- src/functions.php
+++ src/functions.php
@@ -3,3 +3,9 @@
 function between_inclusive($number, $lo, $hi) {
     return $number >= $lo && $number <= $hi;
 }
+
+function curry(callable $callback, ...$initialArgs): callable {
+    return static function (...$args) use ($callback, $initialArgs) {
+        return call_user_func_array($callback, array_merge($initialArgs, $args));
+    };
+}
